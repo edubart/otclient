ae6cd41819535f038434552466158b1e624ce2fe
Eduardo Bart <edub4rt@gmail.com>
2011-10-31 11:24:57 -0200

    remove unused allocator

--- src/framework/graphics/texture.cpp
+++ src/framework/graphics/texture.cpp
@@ -61,6 +61,7 @@ uint Texture::internalLoadGLTexture(uchar *pixels, int channels, int width, int 
         logError("loading texture with size ", width, "x", height, " failed, "
                  "the maximum size allowed by the graphics card is ", maxTexSize, "x", maxTexSize, ",",
                  "to prevent crashes the texture will be displayed as a blank texture");
+        //TODO: a workground, like bilinear scaling the texture
         return 0;
     }
 
