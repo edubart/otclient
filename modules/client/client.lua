08fbedce3804de9b83391c476deae57963bb789a
Eduardo Bart <edub4rt@gmail.com>
2011-12-30 04:36:10 -0200

    fix fullscreen mode on ARM

--- modules/client/client.lua
+++ modules/client/client.lua
@@ -2,6 +2,8 @@ Client = { }
 
 -- TODO: load and save configurations
 function Client.init()
+  g_window.show()
+
   -- initialize in fullscreen mode on mobile devices
   if g_window.getPlatformType() == "X11-EGL" then
     g_window.setFullscreen(true)
@@ -9,6 +11,7 @@ function Client.init()
     g_window.move({ x=220, y=220 })
     g_window.resize({ width=800, height=480 })
   end
+
   g_window.setTitle('OTClient')
   g_window.setIcon('clienticon.png')
   return true
