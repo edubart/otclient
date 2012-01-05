4620f71444843c06ee8a78a0182b02dcb8234b53
Eduardo Bart <edub4rt@gmail.com>
2012-01-05 16:31:52 -0200

    fix game losing focus, handle enter/escape on add vip window

--- modules/addon_playground/playground.lua
+++ modules/addon_playground/playground.lua
@@ -1,11 +1,2 @@
 -- place any code for testing purposes here
-
-function init()
-  local box = createWidget('ComboBox')
-  box:moveTo({x=100, y=8})
-  box:addOption('Option 1')
-  box:addOption('Option 2')
-  displayUI(box)
-end
-
-addEvent(init)
+--scheduleEvent(function() dumpWidgets() end, 100)
