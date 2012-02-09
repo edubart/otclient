8545d15304825944333fd052384ffe5d454a5be1
AndreFaramir <andrefaramir@gmail.com>
2012-02-09 03:27:29 -0200

    implementing battle window, need some improvements but its working

--- modules/game/creature.lua
+++ modules/game/creature.lua
@@ -23,53 +23,74 @@ EmblemGreen = 1
 EmblemRed = 2
 EmblemBlue = 3
 
-function Creature:onSkullChange(skullId)
+function getSkullImagePath(skullId)
   if skullId == SkullYellow then
-    self:setSkullTexture(resolvepath('images/skull_yellow.png'))
+    return 'images/skull_yellow.png'
   elseif skullId == SkullGreen then
-    self:setSkullTexture(resolvepath('images/skull_green.png'))
+    return 'images/skull_green.png'
   elseif skullId == SkullWhite then
-    self:setSkullTexture(resolvepath('images/skull_white.png'))
+    return 'images/skull_white.png'
   elseif skullId == SkullRed then
-    self:setSkullTexture(resolvepath('images/skull_red.png'))
+    return 'images/skull_red.png'
   elseif skullId == SkullBlack then
-    self:setSkullTexture(resolvepath('images/skull_black.png'))
+    return 'images/skull_black.png'
   elseif skullId == SkullOrange then
-    self:setSkullTexture(resolvepath('images/skull_orange.png'))
+    return 'images/skull_orange.png'
+  end
+end
+
+function getShieldImagePathAndBlink(shieldId)
+  if shieldId == ShieldWhiteYellow then
+    return 'images/shield_yellow_white.png', false
+  elseif shieldId == ShieldWhiteBlue then
+    return 'images/shield_blue_white.png', false
+  elseif shieldId == ShieldBlue then
+    return 'images/shield_blue.png', false
+  elseif shieldId == ShieldYellow then
+    return 'images/shield_yellow.png', false
+  elseif shieldId == ShieldBlueSharedExp then
+    return 'images/shield_blue_shared.png', false
+  elseif shieldId == ShieldYellowSharedExp then
+    return 'images/shield_yellow_shared.png', false
+  elseif shieldId == ShieldBlueNoSharedExpBlink then
+    return 'images/shield_blue_not_shared.png', true
+  elseif shieldId == ShieldYellowNoSharedExpBlink then
+    return 'images/shield_yellow_not_shared.png', true
+  elseif shieldId == ShieldBlueNoSharedExp then
+    return 'images/shield_blue_not_shared.png', false
+  elseif shieldId == ShieldYellowNoSharedExp then
+    return 'images/shield_yellow_not_shared.png', false
+  end
+end
+
+function getEmblemImagePath(emblemId)
+  if emblemId == EmblemGreen then
+    return 'images/emblem_green.png'
+  elseif emblemId == EmblemRed then
+    return 'images/emblem_red.png'
+  elseif emblemId == EmblemBlue then
+    return 'images/emblem_blue.png'
+  end
+end
+
+function Creature:onSkullChange(skullId)
+  local imagePath = getSkullImagePath(skullId)
+  if imagePath then
+    self:setSkullTexture(resolvepath(imagePath))
   end
 end
 
 function Creature:onShieldChange(shieldId)
-  if shieldId == ShieldWhiteYellow then
-    self:setShieldTexture(resolvepath('images/shield_yellow_white.png'), false)
-  elseif shieldId == ShieldWhiteBlue then
-    self:setShieldTexture(resolvepath('images/shield_blue_white.png'), false)
-  elseif shieldId == ShieldBlue then
-    self:setShieldTexture(resolvepath('images/shield_blue.png'), false)
-  elseif shieldId == ShieldYellow then
-    self:setShieldTexture(resolvepath('images/shield_yellow.png'), false)
-  elseif shieldId == ShieldBlueSharedExp then
-    self:setShieldTexture(resolvepath('images/shield_blue_shared.png'), false)
-  elseif shieldId == ShieldYellowSharedExp then
-    self:setShieldTexture(resolvepath('images/shield_yellow_shared.png'), false)
-  elseif shieldId == ShieldBlueNoSharedExpBlink then
-    self:setShieldTexture(resolvepath('images/shield_blue_not_shared.png'), true)
-  elseif shieldId == ShieldYellowNoSharedExpBlink then
-    self:setShieldTexture(resolvepath('images/shield_yellow_not_shared.png'), true)
-  elseif shieldId == ShieldBlueNoSharedExp then
-    self:setShieldTexture(resolvepath('images/shield_blue_not_shared.png'), false)
-  elseif shieldId == ShieldYellowNoSharedExp then
-    self:setShieldTexture(resolvepath('images/shield_yellow_not_shared.png'), false)
+  local imagePath, blink = getShieldImagePathAndBlink(shieldId)
+  if imagePath then
+    self:setShieldTexture(resolvepath(imagePath), blink)
   end
 end
 
 function Creature:onEmblemChange(emblemId)
-  if emblemId == EmblemGreen then
-    self:setEmblemTexture(resolvepath('images/emblem_green.png'))
-  elseif emblemId == EmblemRed then
-    self:setEmblemTexture(resolvepath('images/emblem_red.png'))
-  elseif emblemId == EmblemBlue then
-    self:setEmblemTexture(resolvepath('images/emblem_blue.png'))
+  local imagePath = getEmblemImagePath(emblemId)
+  if imagePath then
+    self:setEmblemTexture(resolvepath(imagePath))
   end
 end
 
--- modules/game/game.lua
+++ modules/game/game.lua
@@ -27,6 +27,12 @@ local function onUseWithMouseRelease(self, mousePosition, mouseButton)
         end
       elseif clickedWidget:getClassName() == 'UIItem' and not clickedWidget:isVirtual() then
         g_game.useWith(g_game.selectedThing, clickedWidget:getItem())
+      elseif clickedWidget.isBattleButton or clickedWidget.battleButtonChild then
+        local battleButton = clickedWidget
+        if clickedWidget.battleButtonChild then
+          battleButton = clickedWidget:getParent()
+        end
+        g_game.useWith(g_game.selectedThing, battleButton.creature)
       end
     end
   end
