04c2e1d245c1beb91fbdb9dbd10a943eea090239
AndreFaramir <andrefaramir@gmail.com>
2012-04-11 14:13:25 -0300

    moving report to addons

--- modules/otclientrc.lua
+++ modules/otclientrc.lua
@@ -2,15 +2,3 @@
 -- you can place any custom user code here
 
 Keyboard.bindKeyDown('Ctrl+R', Client.reloadScripts)
-
--- client usage report
-local reportEvent
-local reportMessage = g_app.getVersion() .. ";" .. g_app.getBuildRevision() .. ";" .. g_game.getProtocolVersion() .. ";" .. g_app.getBuildDate()
-function reportClientUsage()
-  if g_game.isOnline() and EnterGame.host and (EnterGame.host:lower():find('trpgb') or EnterGame.host:lower():find('tibiarpgbrasil')) then
-    g_game.talk('!otc-report ' .. reportMessage)
-  end
-  removeEvent(reportEvent)
-  reportEvent = scheduleEvent(reportClientUsage, 5*60000)
-end
-connect(g_game, { onLogin = reportClientUsage } )
