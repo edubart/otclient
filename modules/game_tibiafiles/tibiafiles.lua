7cd49712f8d3336618d25d429d6c0b6f20867f09
Eduardo Bart <edub4rt@gmail.com>
2012-07-18 04:49:44 -0300

    Add missing file

---
+++ modules/game_tibiafiles/tibiafiles.lua
@@ -0,0 +1,13 @@
+TibiaFiles = {}
+
+function TibiaFiles.init()
+  if not g_things.loadDat('/game_tibiafiles/Tibia.dat') then
+    fatal(tr("Unable to load dat file, please place a valid Tibia dat in modules/game_tibiafiles/Tibia.dat"))
+  end
+  if not g_sprites.loadSpr('/game_tibiafiles/Tibia.spr') then
+    fatal(tr("Unable to load spr file, please place a valid Tibia spr in modules/game_tibiafiles/Tibia.spr"))
+  end
+end
+
+function TibiaFiles.terminate()
+end
