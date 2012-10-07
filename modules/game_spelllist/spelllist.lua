a83be17bfee603ae100d025dd9da1ff24a6150ec
Samuel <sum9900@web.de>
2012-10-07 04:12:49 +0200

    Revert "Spell List: Small tweaks"
    This reverts commit 261dd40b963304321a60f6e8f742a83f5f92f947.

--- modules/game_spelllist/spelllist.lua
+++ modules/game_spelllist/spelllist.lua
@@ -136,8 +136,7 @@ function init()
   g_keyboard.bindKeyPress('Down', function() spellList:focusNextChild(KeyboardFocusReason) end, spelllistWindow)
   g_keyboard.bindKeyPress('Up', function() spellList:focusPreviousChild(KeyboardFocusReason) end, spelllistWindow)
 
-  for i = 1, #spellDisplayOrder do
-	local spell = spellDisplayOrder[i]
+  for index, spell in ipairs(spellDisplayOrder) do
     local info = SpellInfo[spell]
     local tmpLabel = g_ui.createWidget('SpellListLabel', spellList)
     tmpLabel:setId(spell)
@@ -164,11 +163,6 @@ end
 function terminate()
   disconnect(g_game, { onGameStart = setupOptions,
                        onGameEnd = resetWindow })
-
-  disconnect(spellList, { onChildFocusChange = function(self, focusedChild)
-                          if focusedChild == nil then return end
-                          updateSpellInformation(focusedChild)
-                        end })   
 
   spelllistButton:destroy()
   spelllistButton      = nil
