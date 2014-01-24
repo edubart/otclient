9da018fdffb7e71fc2a49d51f8e460fa1b14b8ec
conde2 <joao_pc23@hotmail.com>
2014-01-24 00:19:08 -0200

    Temporary fix for async
    This should be fixed in server side, until that we just don't need to
    use.

--- src/client/protocolgameparse.cpp
+++ src/client/protocolgameparse.cpp
@@ -2008,7 +2008,9 @@ ItemPtr ProtocolGame::getItem(const InputMessagePtr& msg, int id)
         if(item->getAnimationPhases() > 1) {
             // 0xfe => random phase
             // 0xff => async?
-            item->setAsync(msg->getU8() == 0xff);
+            msg->getU8();
+            // Remove async since OTB file in server is configured so every item with animationPhase > 1 is a random phase item.
+            //item->setAsync(msg->getU8() == 0xff);
         }
     }
 
