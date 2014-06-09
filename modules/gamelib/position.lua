ce9e443c606c8715dfc7d5d99ade6e31e71333d9
BenDol <dolb90@gmail.com>
2014-06-10 01:44:03 +1200

    Remove redundant classes & added some Position methods.

--- modules/gamelib/position.lua
+++ modules/gamelib/position.lua
@@ -23,3 +23,15 @@ end
 function Position.isInRange(pos1, pos2, xRange, yRange)
   return math.abs(pos1.x-pos2.x) <= xRange and math.abs(pos1.y-pos2.y) <= yRange and pos1.z == pos2.z;
 end
+
+function Position.isValid(pos)
+  return not (pos.x == 65535 and pos.y == 65535 and pos.z == 255)
+end
+
+function Position.distance(pos1, pos2)
+  return math.sqrt(math.pow((pos2.x - pos1.x), 2) + math.pow((pos2.y - pos1.y), 2))
+end
+
+function Position.manhattanDistance(pos1, pos2)
+  return math.abs(pos2.x - pos1.x) + math.abs(pos2.y - pos1.y)
+end
