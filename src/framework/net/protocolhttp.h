78fd2d858fd2fb669e504d26c47f033191add1fa
Shawak <Shawak@users.noreply.github.com>
2014-07-31 18:55:49 +0200

    Update protocolhttp.h

--- src/framework/net/protocolhttp.h
+++ src/framework/net/protocolhttp.h
@@ -40,7 +40,7 @@ public:
 
     void send(const std::string &message);
     void recv();
-	void recv_some();
+    void recv_some();
 
     ProtocolHttpPtr asProtocolHttp() { return static_self_cast<ProtocolHttp>(); }
 
