4807c4a19db0b5f460d4d5975f0ddf3047006f0f
Túlio Henrique <tuliohdvp@gmail.com>
2015-01-02 07:31:58 -0200

    Removed the line that delete the build folder

--- android/compile_android.sh
+++ android/compile_android.sh
@@ -4,7 +4,7 @@ mkdir ../build && cd ../build
 cmake -DCMAKE_TOOLCHAIN_FILE=../android/android.toolchain.cmake -DANDROID_ABI=armeabi-v7a -DANDROID_NATIVE_API_LEVEL=android-16 -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-4.6 ..
 make
 
-cd ../ && rm -r build
+cd ../
 
 cp -r libs android/project/ && rm -r libs
 
