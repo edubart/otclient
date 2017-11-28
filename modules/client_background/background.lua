78ca89d2b4b6af6dbb05056388560574c6e1e9c4
EgzoT <przemek7575@prokonto.pl>
2017-11-29 00:42:15 +0100

    Add compiler info to version label in start screen
    Add compiler name and version to start screen (client_background module).
    It would help distinguish OTClient problems on different compilations.

--- modules/client_background/background.lua
+++ modules/client_background/background.lua
@@ -10,7 +10,7 @@ function init()
   clientVersionLabel = background:getChildById('clientVersionLabel')
   clientVersionLabel:setText(g_app.getName() .. ' ' .. g_app.getVersion() .. '\n' ..
                              'Rev  ' .. g_app.getBuildRevision() .. ' ('.. g_app.getBuildCommit() .. ')\n' ..
-                             'Built on ' .. g_app.getBuildDate())
+                             'Built on ' .. g_app.getBuildDate() .. '\n' .. g_app.getBuildCompiler())
 
   if not g_game.isOnline() then
     addEvent(function() g_effects.fadeIn(clientVersionLabel, 1500) end)
