251c937b7e5602592e60576e3f4fa1bc2ea4a44f
David Kennedy S. A <daviduser.guj@gmail.com>
2018-03-21 01:56:38 -0300

    Disable SE Linux when running the client on container

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+SE_enforcing=`getenforce` || true
+
+sudo setenforce Permissive || true
+
 docker run -ti --rm \
        -e DISPLAY \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        --device /dev/dri \
        edubart/otclient 
+
+sudo setenforce $SE_enforcing || true
