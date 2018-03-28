d03ffc1f26be3171a3673fa73fedb85664ab6cad
David Kennedy S. A <daviduser.guj@gmail.com>
2018-03-28 11:57:59 -0300

    Enable any client connect to X host

--- run.sh
+++ run.sh
@@ -4,10 +4,16 @@ SE_enforcing=`getenforce` || true
 
 sudo setenforce Permissive || true
 
+# Enable any host to connect on X Org
+xhost +
+
 docker run -ti --rm \
        -e DISPLAY \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        --device /dev/dri \
        edubart/otclient 
+
+# Enable any host to connect on X Org
+xhost -
 
 sudo setenforce $SE_enforcing || true
