c285d2def78c2b9be1fc5fc167a4451061d08166
Ascuas Funkeln <64284398+AscuasFunkeln@users.noreply.github.com>
2020-05-03 20:13:18 +0200

    Update skills.lua

--- modules/game_skills/skills.lua
+++ modules/game_skills/skills.lua
@@ -1,11 +1,6 @@
 skillsWindow = nil
 skillsButton = nil
 
-function comma_value(n)
-	local left,num,right = string.match(n,'^([^%d]*%d)(%d*)(.-)$')
-	return left..(num:reverse():gsub('(%d%d%d)','%1,'):reverse())..right
-end
-
 function init()
   connect(LocalPlayer, {
     onExperienceChange = onExperienceChange,
