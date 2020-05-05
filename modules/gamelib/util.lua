83ad17e7126ae2028db84f9b9fe94f28f12821ad
Ascuas Funkeln <64284398+AscuasFunkeln@users.noreply.github.com>
2020-05-05 17:40:04 +0200

    Update util.lua

--- modules/gamelib/util.lua
+++ modules/gamelib/util.lua
@@ -9,6 +9,7 @@ function dirtostring(dir)
     end
   end
 end
+
 function comma_value(n)
 	local left,num,right = string.match(n,'^([^%d]*%d)(%d*)(.-)$')
 	return left..(num:reverse():gsub('(%d%d%d)','%1,'):reverse())..right
