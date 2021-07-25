7c36977935c58256defdf1d3c78a207f66bd9566
scopp <osc.car2@gmail.com>
2021-07-25 19:09:48 +0200

    Small parenthesis fix

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -596,7 +596,7 @@ bool Game::walk(const Otc::Direction direction, bool isKeyDown /*= false*/)
     // check we can walk and add new walk event if false
     if(!m_localPlayer->canWalk(direction)) {
         const float ticks = stdext::clamp<float>(m_localPlayer->getStepTicksLeft(), 1, 2000);
-        if((m_nextScheduledDir != direction && isKeyDown) || ticks < std::min<int>(250, m_localPlayer->getStepDuration()/4)) {
+        if(m_nextScheduledDir != direction && (isKeyDown || ticks < std::min<int>(250, m_localPlayer->getStepDuration()/4))) {
             // must add a new walk event
             if(m_walkEvent) {
                 m_walkEvent->cancel();
