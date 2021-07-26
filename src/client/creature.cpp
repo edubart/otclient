a78d99cc3be0f2056abe9e9457b3437e15784144
scopp <osc.car2@gmail.com>
2021-07-26 11:04:15 +0200

    Possible fix for animation reset in #112

--- src/client/creature.cpp
+++ src/client/creature.cpp
@@ -697,8 +697,6 @@ void Creature::setOutfit(const Outfit& outfit)
         m_outfit = outfit;
     }
 
-    m_walkAnimationPhase = 0; // might happen when player is walking and outfit is changed.
-
     callLuaField("onOutfitChange", m_outfit, oldOutfit);
 
     // Cache
