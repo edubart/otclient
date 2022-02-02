250d766f9be6f71584d7458c1f8a1b3129cb1edb
nekiro <nekiro.dev@gmail.com>
2022-02-02 19:55:56 +0100

    Change last supported version to 1285

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -1478,7 +1478,7 @@ void Game::setProtocolVersion(int version)
     if(isOnline())
         stdext::throw_exception("Unable to change protocol version while online");
 
-    if(version != 0 && (version < 740 || version > 1281))
+    if(version != 0 && (version < 740 || version > 1285))
         stdext::throw_exception(stdext::format("Protocol version %d not supported", version));
 
     m_protocolVersion = version;
@@ -1496,7 +1496,7 @@ void Game::setClientVersion(int version)
     if(isOnline())
         stdext::throw_exception("Unable to change client version while online");
 
-    if(version != 0 && (version < 740 || version > 1281))
+    if(version != 0 && (version < 740 || version > 1285))
         stdext::throw_exception(stdext::format("Client version %d not supported", version));
 
     m_features.reset();
