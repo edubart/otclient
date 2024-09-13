b507c901182f5312e50941bbf64d19c81940cf55
divinity76 <hans@loltek.net>
2024-09-13 09:26:07 +0200

    Make outputMessage::addRawString available in Lua
    Why write
    ```lua
    msg:addU8(1);
    msg:addU8(0);
    msg:addU8(113);
    ```
    when you can instead write
    ```lua
    msg:addRawString("\x01\x00\x71");
    ```
    ? Would make some lua scripts much easier to write :)

--- src/framework/luafunctions.cpp
+++ src/framework/luafunctions.cpp
@@ -818,6 +818,7 @@ void Application::registerLuaFunctions()
     g_lua.bindClassMemberFunction<OutputMessage>("addU32", &OutputMessage::addU32);
     g_lua.bindClassMemberFunction<OutputMessage>("addU64", &OutputMessage::addU64);
     g_lua.bindClassMemberFunction<OutputMessage>("addString", &OutputMessage::addString);
+    g_lua.bindClassMemberFunction<OutputMessage>("addRawString", &OutputMessage::addRawString);
     g_lua.bindClassMemberFunction<OutputMessage>("addPaddingBytes", &OutputMessage::addPaddingBytes);
     g_lua.bindClassMemberFunction<OutputMessage>("encryptRsa", &OutputMessage::encryptRsa);
     g_lua.bindClassMemberFunction<OutputMessage>("getMessageSize", &OutputMessage::getMessageSize);
